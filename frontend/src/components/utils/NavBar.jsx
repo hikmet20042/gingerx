c701efa4081db276ad4c84b1b7b01a710533c797
Memmedli Hikmet <memmedlih032@gmail.com>
2023-03-31 15:10:50 +0400

    small error

--- frontend/src/components/utils/NavBar.jsx
+++ frontend/src/components/utils/NavBar.jsx
@@ -56,7 +56,7 @@ export default function NavBar() {
               Profile
             </Link>
           ) : (
-            !window.location == "https://gingerx.netlify.app/login" && (
+            !window.location === "https://gingerx.netlify.app/login" && (
               <Link className="nav-item" to="/login">
                 Log in
               </Link>
